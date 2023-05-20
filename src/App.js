e05587f230a202a41a8c4fdb6a76b26bb88f21e5
Humna Hashmi <hashmihumna57@gmail.com>
2023-05-20 15:19:16 +0500

    Frontend UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Hedaer from "./components/Hedaer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
 function App() {
   return (
--- src/components/Hedaer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Hedaer() {
-  return (
-    <div>Hedaer</div>
-  )
-}
-
-export default Hedaer
